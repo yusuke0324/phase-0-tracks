7957aa48a2431e508c8a3eec56586d6d238663ea
yusuke0324 <take.yusuke@gmail.com>
2016-11-08 12:03:35 +0900

    add puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,12 @@
+# Puppy ------------------------
+# Name
+
+# CHARACTERISTICS ----------------
+# Color:
+# Size: yes
+# Length of fur:
+
+# BEHAVIOR -----------------------
+# Jump
+# Brark
+# Eat
