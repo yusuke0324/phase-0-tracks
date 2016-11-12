03909bbc90e1643ac937870a967f9462ec791cb5
yusuke0324 <take.yusuke@gmail.com>
2016-11-12 22:42:43 +0900

    changed shout module to mixin and make guman and alien class

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,12 +1,46 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happilly(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# p Shout.yell_angrily("yes")
+# p Shout.yelling_happilly("yes")
+
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happilly(words)
+  def yelling_happilly(words)
     words + "!!!" + " :)"
   end
 end
 
-p Shout.yell_angrily("yes")
-p Shout.yelling_happilly("yes")
+class Human
+  include Shout
+end
+
+
+class Alien
+  include Shout
+end
+
+alien = Alien.new
+p alien.yell_angrily("fkldr;::*")
+p alien.yelling_happilly("fkldr;::*")
+
+
+human = Human.new
+p human.yell_angrily("yes")
+p human.yelling_happilly("yes")
+
+
+
+
+
